268b833645de3b5812f5125bfd45b0b35f328830
Thomas the Franque <tfranque@gmail.com>
2020-04-19 17:31:15 +0100

    Animationwopwo[

---
+++ Assets/Scripts/UI/InitLeanTween.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InitLeanTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        LeanTween.init(1000);
+        Destroy(this);
+    }
+}
