515a6f422acdd217f9de445d9dcb8b01eca4eb84
Thomas the Franque <tfranque@gmail.com>
2020-04-19 14:37:12 +0100

    Readd button control in inspector

--- Assets/Scripts/Editor/WALTDebuggerInspector.cs
+++ Assets/Scripts/Editor/WALTDebuggerInspector.cs
@@ -11,8 +11,10 @@ namespace WALTApp
         public override void OnInspectorGUI()
         {
             _debugger = (WALTDebugger)target;
-            
             DrawDefaultInspector();
+
+            if (!Application.isPlaying) return;
+            
             EditorGUILayout.LabelField("", GUILayout.Height(3));
             if (GUILayout.Button("Send Message", GUILayout.Height(50)))
                 if (!_debugger.Send()) Debug.LogWarning("Message not sent.");
